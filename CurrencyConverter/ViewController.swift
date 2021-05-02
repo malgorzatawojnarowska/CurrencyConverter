8bc5b27d0bb05661dc417e9f7829d0ece90d0fa6
malgorzatawojnarowska <malgorzatawojnarowska@outlook.com>
2021-05-02 20:54:14 +0200

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Małgorzata Wojnarowska on 02/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
